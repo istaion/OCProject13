9b710a988cae090acc905df0e12ddcbbf1c253f3
istaion <v.poutot@gmail.com>
2023-06-28 10:11:16 +0200

    copie de profile

--- oc_lettings_site/admin.py
+++ oc_lettings_site/admin.py
@@ -4,7 +4,8 @@ from .models import Letting as OldLetting
 from .models import Address as OldAddress
 from lettings.models import Letting
 from lettings.models import Address
-from .models import Profile
+from .models import Profile as OldProfile
+from profiles.models import Profile
 
 
 admin.site.register(Letting)
@@ -12,3 +13,4 @@ admin.site.register(Address)
 admin.site.register(OldLetting)
 admin.site.register(OldAddress)
 admin.site.register(Profile)
+admin.site.register(OldProfile)
