3deb9ece8b8e0661319e29304b5e29e1645657b7
istaion <v.poutot@gmail.com>
2023-06-28 10:01:17 +0200

    copie sur lettings

--- oc_lettings_site/admin.py
+++ oc_lettings_site/admin.py
@@ -1,10 +1,14 @@
 from django.contrib import admin
 
-from .models import Letting
-from .models import Address
+from .models import Letting as OldLetting
+from .models import Address as OldAddress
+from lettings.models import Letting
+from lettings.models import Address
 from .models import Profile
 
 
 admin.site.register(Letting)
 admin.site.register(Address)
+admin.site.register(OldLetting)
+admin.site.register(OldAddress)
 admin.site.register(Profile)
