99a0eb6123c46ba78d6acd8bebe9a8342581ea97
istaion <v.poutot@gmail.com>
2023-06-28 10:40:16 +0200

    refactorisation complete

--- oc_lettings_site/admin.py
+++ oc_lettings_site/admin.py
@@ -1,16 +1,11 @@
 from django.contrib import admin
 
-from .models import Letting as OldLetting
-from .models import Address as OldAddress
 from lettings.models import Letting
 from lettings.models import Address
-from .models import Profile as OldProfile
 from profiles.models import Profile
 
 
 admin.site.register(Letting)
 admin.site.register(Address)
-admin.site.register(OldLetting)
-admin.site.register(OldAddress)
 admin.site.register(Profile)
-admin.site.register(OldProfile)
+
