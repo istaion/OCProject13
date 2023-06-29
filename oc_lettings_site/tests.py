126d034ffcd2957d3aff12745cea8ca7ef3f4128
istaion <v.poutot@gmail.com>
2023-06-29 14:49:57 +0200

    problème sur les tests résolu

--- oc_lettings_site/tests.py
+++ oc_lettings_site/tests.py
@@ -1,2 +1,10 @@
-def test_dummy():
-    assert 1
+from django.test import TestCase
+from django.urls import reverse
+
+
+class IndexTest(TestCase):
+
+    def test_index(self):
+        response = self.client.get(reverse('index'))
+        assert response.status_code == 200
+        assert b'Welcome to Holiday Homes' in response.content
