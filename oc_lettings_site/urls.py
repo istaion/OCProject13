39252d09cecfc289190974369758246668826487
istaion <v.poutot@gmail.com>
2023-08-15 10:16:03 +0200

    ajout d'erreur sentry

--- oc_lettings_site/urls.py
+++ oc_lettings_site/urls.py
@@ -3,11 +3,19 @@ from django.urls import path, include
 
 from . import views
 
+
+def trigger_error(request):
+    division_by_zero = 1 / 0
+    print(division_by_zero)
+
+
 urlpatterns = [
     path('', views.index, name='index'),
     path('lettings/', include('lettings.urls')),
     path('profiles/', include('profiles.urls')),
     path('admin/', admin.site.urls),
+    path('sentry-debug/', trigger_error),
 ]
+
 
 handler500 = views.error_500
